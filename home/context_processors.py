3265ab59a9b1d9803e7d1bb58c4d6317543e2291
Daniel Nogueira <daniellnogueira@hotmail.com>
2026-08-20 14:47:57 +0100

    retirada da loja sem produto

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,9 +1,11 @@
 from .cart import cart_count
 from .constants import INSTAGRAM_URL
+from .models import Produto
 
 
 def site(request):
     return {
         "instagram_url": INSTAGRAM_URL,
         "cart_count": cart_count(request.session),
+        "loja_disponivel": Produto.objects.filter(ativo=True).exists(),
     }
